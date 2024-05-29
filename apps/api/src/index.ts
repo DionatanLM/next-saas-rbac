6480fb2e17f5ad1b80081dfd727dd0ffd1854198
Dionatan de Lima <dionatanlm12271@gmail.com>
2024-05-29 13:06:36 -0300

    initial commit

---
+++ apps/api/src/index.ts
@@ -0,0 +1,10 @@
+import { defineAbilitiesFor } from '@saas/auth'
+
+const ability = defineAbilitiesFor({
+  role: 'ADMIN',
+})
+
+const userCanInviteSomeoneElse = ability.can('invite', 'User')
+
+console.log(userCanInviteSomeoneElse) // true
+
